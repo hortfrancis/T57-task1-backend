e7b2e48728f28e837ea9e61d96b87095615f2a0e
Alex Hort-Francis <alex.hortfrancis@gmail.com>
2023-03-15 19:14:12 +0000

    Add Mongo Atlas credentials to app.js

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+require('dotenv').config();
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+// Get MongoDB Atlas credentials from .env file
+const MONGO_USERNAME = process.env.MONGO_USERNAME; 
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+
 // Connection string for MongoDB Atlas database
-const URI = 'mongodb+srv://ahf23:<password>@hyperion-dev-1234.hiukfbn.mongodb.net/?retryWrites=true&w=majority';
+const URI = 'mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@hyperion-dev-1234.hiukfbn.mongodb.net/?retryWrites=true&w=majority';
