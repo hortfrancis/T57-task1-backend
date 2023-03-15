58b06443b9a99bc233ff6cba33b92929f4e02e71
Alex Hort-Francis <alex.hortfrancis@gmail.com>
2023-03-15 20:07:05 +0000

    Add connection to Mongo Atlas database

--- app.js
+++ app.js
@@ -11,4 +11,14 @@ const MONGO_USERNAME = process.env.MONGO_USERNAME;
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
 
 // Connection string for MongoDB Atlas database
-const URI = 'mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@hyperion-dev-1234.hiukfbn.mongodb.net/?retryWrites=true&w=majority';
+const MONGO_URI = 'mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@hyperion-dev-1234.hiukfbn.mongodb.net/?retryWrites=true&w=majority';
+
+// Options object as 2nd argument for better long-term compatibility with MongoDB Atlas
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+  )
+  // Promise-based approach is modern and recommended
+    .then(() => console.log('Connection to MongoDB Atlas successful!'))
+    .catch((err) => console.error('Error connecting to the database:', err));
