59a730af439ed1a4f7d8e5b38d77f17d75a0e62c
Alex Hort-Francis <alex.hortfrancis@gmail.com>
2023-03-15 18:47:40 +0000

    Add dotenv as a dependency

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+// Connection string for MongoDB Atlas database
+const URI = 'mongodb+srv://ahf23:<password>@hyperion-dev-1234.hiukfbn.mongodb.net/?retryWrites=true&w=majority';
