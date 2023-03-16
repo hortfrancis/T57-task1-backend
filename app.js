82ddba7217622da72568b5acf9c9eeb0075f6223
Alex Hort-Francis <alex.hortfrancis@gmail.com>
2023-03-16 12:50:04 +0000

    Set up Express server with GET endpoint

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Get MongoDB Atlas credentials from .env file
-const MONGO_USERNAME = process.env.MONGO_USERNAME; 
+const MONGO_USERNAME = process.env.MONGO_USERNAME;
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
 
 // Connection string for MongoDB Atlas database
@@ -15,10 +15,23 @@ const MONGO_URI = 'mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@hyperion-de
 
 // Options object as 2nd argument for better long-term compatibility with MongoDB Atlas
 mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-  )
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}
+)
   // Promise-based approach is modern and recommended
-    .then(() => console.log('Connection to MongoDB Atlas successful!'))
-    .catch((err) => console.error('Error connecting to the database:', err));
+  .then(() => console.log('Connection to MongoDB Atlas successful!'))
+  .catch((err) => console.error('Error connecting to the database:', err));
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening for HTTP requests on port ${PORT}.`);
+});
